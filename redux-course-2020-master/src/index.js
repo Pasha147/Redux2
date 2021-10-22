d66523c0ecbd1ad1ab35f13127926d2b6dde598b
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-10-22 21:53:09 +0300

    Redux without redux only js

--- redux-course-2020-master/src/index.js
+++ redux-course-2020-master/src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css";
+import { createStore } from "./createStore";
+import { rootReducer } from "./redux/rootReducer";
 
 const counter = document.getElementById("counter");
 const addBtn = document.getElementById("add");
@@ -6,27 +8,24 @@ const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-let state = 1;
-
-function render() {
-  counter.textContent = state.toString();
-}
+const store = createStore(rootReducer, 0);
+// window.store = store;
 
 addBtn.addEventListener("click", () => {
-  state++;
-  render();
+  store.dispatch({ type: "INCREMENT" });
 });
 subBtn.addEventListener("click", () => {
-  state--;
-  render();
+  store.dispatch({ type: "DECREMENT" });
 });
-asyncBtn.addEventListener("click", () => {
-  setTimeout(() => {
-    state++;
-    render();
-  }, 2000);
+asyncBtn.addEventListener("click", () => {});
+
+store.subscribe(() => {
+  const state = store.getState();
+  counter.textContent = state;
 });
+
+store.dispatch({ type: "INIT_APP" });
+
 themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  // document.body.classList.toggle("dark");
 });
-render();
